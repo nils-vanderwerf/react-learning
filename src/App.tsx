3eb37293d0b2784aebfa6e366f22f5fab0411f50
Nils van der Werf <n.vanderw.92@gmail.com>
2026-03-23 11:31:35 +1100

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import Message from "./components/message";
+
+function App() {
+  return ( 
+  <>
+    <h1>React app</h1>
+    <Message/>
+  </> 
+)
+}
+export default App;
---
+++ src/components/message.tsx
@@ -0,0 +1,5 @@
+function Message() {
+  return <p>Hello from message component</p>
+}
+
+export default Message 
