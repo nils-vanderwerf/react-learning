b1a7bc57d33b0a005f545ab177c789649168ecd4
Nils van der Werf <n.vanderw.92@gmail.com>
2026-03-25 15:29:22 +1100

    Experiment with useState

--- src/components/languages.tsx
+++ src/components/languages.tsx
@@ -1,15 +1,22 @@
+import { useState  } from "react"
 function Languages() {
   const languages = ["Javascript", "PHP", "Python", "Swift"]
 
-  const handleClick = (language) => {
-    alert(`I am clicked! The language clicked was ${language}`)
-  }
+  const [selectedItem, setSelectedItem] = useState(0)
+
+
   return (
     <>
       <h1 className="text-2xl font-extrabold">Programming Languages</h1>
-      <ul>
+      <ul className="w-1/2 border border-gray-200 rounded-lg divide-y divide-gray-300 overflow-hidden">
         {languages.map((language, index) => (
-        <li key={index} onClick={() => handleClick(language)}>{language}</li>
+        <li 
+          key={index}
+          className={selectedItem === index ? 'py-2 px-3 bg-gray-800 text-white' : 'py-2 px-3'}
+          onClick={() => setSelectedItem(index)}
+        >
+          {language}
+        </li>
         ))}
       </ul>
 
